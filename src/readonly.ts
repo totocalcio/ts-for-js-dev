64e427dd38cc898c986f6406e193fb1df3640bfa
totocalcio <totohyper@yahoo.co.jp>
2023-11-21 16:52:25 +0900

    初級

---
+++ src/readonly.ts
@@ -0,0 +1,12 @@
+export {};
+
+class VisaCard {
+  // readonly owner: string;
+
+  constructor(public readonly _owner: string) {}
+  // this.owner = owner;
+}
+
+let myVisaCard = new VisaCard("ととかるちょ");
+console.log(myVisaCard._owner);
+myVisaCard._owner = "ぎんちゃん";
